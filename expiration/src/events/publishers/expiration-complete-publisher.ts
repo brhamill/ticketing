ce95798dda69ccd6644cd4ba4d5f8827ac83cf2f
Brad Hamill <brad@hamill.com>
2020-06-15 19:29:44 -0600

    expiration service complete

--- expiration/src/events/publishers/expiration-complete-publisher.ts
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -2,7 +2,7 @@ import {
   Subjects,
   Publisher,
   ExpirationCompleteEvent,
-} from '@sgtickets/common';
+} from '@bhtickets/common';
 
 export class ExpirationCompletePublisher extends Publisher<
   ExpirationCompleteEvent
