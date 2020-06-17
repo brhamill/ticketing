4302c93c10e36215570375b06d34249c6308580a
Brad Hamill <brad@hamill.com>
2020-06-17 01:17:32 -0600

    code complete

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -37,7 +37,7 @@ const OrderShow = ({ order, currentUser }) => {
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
         token={({ id }) => doRequest({ token: id })}
-        stripeKey="pk_test_FlLFVapGHTly3FicMdTU06SC006tWtWbNH"
+        stripeKey='pk_test_sS9M9xDWlDUDeHJ7DdnQDwyr00LeJSjnjx'
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
